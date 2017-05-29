1dbc97ac78d343f212734673ed0196e051b34df6
sultanuddin002 <shaikh.sultan.uddin@gmail.com>
2017-05-29 17:22:04 +0500

    simple comments

--- src/com/restaurantreserve/ws/Constants.java
+++ src/com/restaurantreserve/ws/Constants.java
@@ -2,6 +2,7 @@ package com.restaurantreserve.ws;
 
 public class Constants {
 
+	// Normal static Constants 
 	public static String dbClass = "com.mysql.jdbc.Driver";
 	public static String dbName = "mydb";
 	public static String dbUrl = "jdbc:mysql://localhost:3306/"+dbName;
